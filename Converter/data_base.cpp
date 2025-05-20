32582f3fd13f81de91781d25a0b16a5b97adf897
SerhiiKnyzhhenko <12345sharkk@gmail.com>
2025-05-20 18:01:27 +0300

    13 commit add funcotion to db class

--- Converter/data_base.cpp
+++ Converter/data_base.cpp
@@ -6,6 +6,58 @@ pqxx::result data_base::new_response(const std::string& response_text) {
 
 }
 
-void data_base::add_to_db(const std::string& currency, double rate) {
+void data_base::add_to_db_from_hash(const std::string& date, const std::unordered_map<std::string, double>& rates) {
+	try
+	{
+		for (const auto& element : rates)
+		{
+			std::string currency = element.first;
+			double rate = element.second;
 
+			worker.exec_params(
+				"INSERT INTO rates (date, currency, rate) VALUES ($1, $2, $3)",
+				date, currency, rate);
+
+			worker.commit();
+		}
+		
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void data_base::add_to_db_row(const std::string& date, const std::string& currency, double rate) {
+	try
+	{
+		if (isDateValid(date)) {
+			worker.exec_params(
+				"INSERT INTO rates (date, currency, rate) VALUES ($1, $2, $3)",
+				date, currency, rate);
+
+			worker.commit();
+		}
+		else {
+			std::cerr << "Date is no valid!" << std::endl;
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+}
+
+std::string data_base::get_current_date() {
+	auto now = std::chrono::system_clock::now();
+	auto today = std::chrono::current_zone()->to_local(now);
+	auto ymd = std::chrono::year_month_day(std::chrono::floor<std::chrono::days>(today));
+
+	return std::format("{:%Y-%m-%d}", ymd);
+}
+
+bool data_base::isDateValid(const std::string& date) {
+	std::regex pattern(R"(\d{4}-\d{2}-\d{2})");
+	return std::regex_match(date, pattern);
 }
--- Converter/data_base.h
+++ Converter/data_base.h
@@ -2,6 +2,10 @@
 #define DATA_BASE_H
 
 #include <pqxx/pqxx>
+#include <iostream>
+#include <chrono>
+#include <format>
+#include <regex>
 
 std::string conn = "host=localhost port=5432 dbname=converter user=postgres password =12345Kikki";
 
@@ -13,8 +17,11 @@ private:
 	data_base();
 	/*data_base(const std::string& date);*/
 	pqxx::result new_response(const std::string&);
-	void add_to_db(const std::string&, double);
-	
+	void add_to_db_from_hash(const std::string&, const std::unordered_map<std::string, double>&);
+	void add_to_db_row(const std::string&, const std::string&, double);
+	std::string get_current_date();
+	bool isDateValid(const std::string&);
+
 };
 
 #endif // !DATA_BASE_H
