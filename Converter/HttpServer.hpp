c0d1232f94583f615e38db8e05f139ac4d7f0905
SerhiiKnyzhhenko <12345sharkk@gmail.com>
2025-05-22 14:16:53 +0300

    24 commit add HttpServer.hpp

---
+++ Converter/HttpServer.hpp
@@ -0,0 +1,44 @@
+#ifndef HTTP_SERVER_HPP
+#define HTTP_SERVER_HPP
+
+#include <vector>
+#include <fstream>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+class HttpServer {
+private: 
+	int port_{ 443 };
+	int socket_{ -1 };
+
+	std::string sertificatePath_;
+	std::string keyPath_;
+	std::string caPath_;
+	std::string htmlData_;
+
+	std::string startDateTime_;
+	size_t getRequestCount_{ 0 };
+
+	SSL_METHOD* ssl_method_{ nullptr };
+	SSL_CTX* ssl_context_{ nullptr };
+
+	std::vector<char> fav_icon_buffer;
+
+private:
+	bool _ssl_init();
+	bool _socket_init();
+	void _client_processing(const std::string&);
+
+public:
+	HttpServer(int port = 443);
+	~HttpServer();
+
+	void _setSertPath(const std::string&);
+	void _setKeyPath(const std::string&);
+	void _setPathForCA(const std::string&);
+};
+
+#endif // !HTTP_SERVER_HPP
+
+
+
